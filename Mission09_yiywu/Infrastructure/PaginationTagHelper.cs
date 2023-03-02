43048197e7c5395b66fb455845d45810d2ae8308
eva2077 <evwu77@gmail.com>
2023-03-02 01:44:23 -0700

    bootstrap

--- Mission09_yiywu/Infrastructure/PaginationTagHelper.cs
+++ Mission09_yiywu/Infrastructure/PaginationTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Mission09_yiywu.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mission09_yiywu.Infrastructure
 {
@@ -22,14 +23,25 @@ namespace Mission09_yiywu.Infrastructure
         //different 
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
             TagBuilder final = new TagBuilder("div");
+
             for (int i = 1; i <= PageBlah.TotalPages; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
--- Mission09_yiywu/Startup.cs
+++ Mission09_yiywu/Startup.cs
@@ -13,9 +13,10 @@ using System.Threading.Tasks;
 
 namespace Mission09_yiywu
 {
+    
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method sets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IConfiguration Configuration { get; set; }
         public Startup(IConfiguration temp)
